3b49fa899e98535f16f0d7646ec3c23cf3f3e521
Vas <VasVadum@Gmail.com>
2018-09-03 03:28:01 -0500

    Initial Upload
    Initial Upload

---
+++ Source/texconvConvert_DDS_to_TIF.bat
@@ -0,0 +1,3 @@
+texconv -ft TIF -if LINEAR -sRGB -y -o TIF_Source\ DDS_Source\*.dds
+ren TIF_Source\*.TIF *.tif
+pause
---
+++ Source/texconvConvert_TIF_to_DDS.bat
@@ -0,0 +1,3 @@
+texconv -f BC7_UNORM -if LINEAR -sRGB -y -o DDS_Output\ TIF_Source\*.tif
+ren DDS_Output\*.DDS *.dds
+pause
