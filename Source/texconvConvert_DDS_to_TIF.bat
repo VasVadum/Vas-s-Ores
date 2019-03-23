145670e00afc373f15ca2101939b388ed5456595
Vas <VasVadum@Gmail.com>
2019-03-22 23:55:49 -0500

    Ore Rebalancing
    A slight rebalancing and an update to add the new Survival Kit to support this.

--- Source/texconvConvert_DDS_to_TIF.bat
+++ Source/texconvConvert_DDS_to_TIF.bat
@@ -1,3 +1,3 @@
-texconv -ft TIF -if LINEAR -sRGB -y -o TIF_Source\ DDS_Source\*.dds
+D:\SteamLibrary\steamapps\common\SpaceEngineers\Tools\TexturePacking\Tools\texconv.exe -ft TIF -if LINEAR -sRGB -y -o TIF_Source\ DDS_Source\*.dds
 ren TIF_Source\*.TIF *.tif
 pause
